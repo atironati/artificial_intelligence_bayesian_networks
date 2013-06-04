d9f7cb7dfaf2e9d1f30020ddd0ed8b33253189d9
Alex Tironati <atironati@gmail.com>
2013-06-04 23:58:03 +1000

    add multiple n value computations, avg, and variance

--- bayesian_network.rb
+++ bayesian_network.rb
@@ -25,11 +25,9 @@ class BayesianNetwork
       return rows
     else
       curr_node = rem_nodes.shift
-      puts curr_node.name
 
       node_values = [{curr_node.name => true},
                      {curr_node.name => false}]
-      puts node_values.inspect
 
       # need to add more nodes in
       if rows.size == 0
@@ -46,8 +44,8 @@ class BayesianNetwork
     end
   end
 
-  def likelihood_weighting(x, events, n)
-    # we want to start iteratinf from the parent nodes
+  def likelihood_weighting(query_x, events, n)
+    # we want to start iterating from the parent nodes
     parent_nodes = get_parent_nodes
 
     # because events are fixed, we can filter out unused entries in weighted_counts
@@ -68,22 +66,26 @@ class BayesianNetwork
     end
 
     n.times do
+      fix_event_values(events)
+
       x,w = weighted_sample(parent_nodes, {}, events, 1.0)
       local_weighted_counts[x] += w
+
+      reset_network_values
     end
 
-    #normalize(weighted_counts, events, n)
+    #puts "WEIGHTED COUNTS"
+    #local_weighted_counts.each do |k, v|
+      #puts "k: #{k},   v: #{v}"
+    #end
+
+    # return an array containing normalized probability of [x = true, x = false] given the events
+    normalize_over(query_x, local_weighted_counts)
   end
 
   # use BFS to traverse tree, select random sample, and update weight value
   def weighted_sample(nodes_to_traverse, traversed_nodes, events, weight)
     if nodes_to_traverse == []
-      puts "traversed_nodes"
-      puts traversed_nodes.inspect
-
-      puts "weight"
-      puts weight.inspect
-
       return [traversed_nodes, weight]
     else
       next_nodes = []
@@ -102,6 +104,10 @@ class BayesianNetwork
           # P( node = true | parents(node) )
           curr_true_prob = node.prob_table[key.merge({node.name => true})]
 
+          #puts "name: #{node.name}"
+          #puts "RAND VALUE: #{rand_num}"
+          #puts "CURR_TRUE_PROB: #{curr_true_prob}"
+
           # store value selection for this node, based on random number selection
           value_selection = nil
           if curr_true_prob > rand_num
@@ -113,10 +119,13 @@ class BayesianNetwork
 
           # set value for this node
           node.value = value_selection
+
+          #puts "VALUE SELECTION: #{value_selection}"
         else
           # store value selection for this node
           traversed_nodes[node.name] = node.value
           # P( node = node.value | parents(node) )
+          #puts "event node #{node.name} weight change: #{key.merge({node.name => node.value})}"
           curr_prob = node.prob_table[key.merge({node.name => node.value})]
 
           # update weights if this is an event node (it should be, but I am checking just in case)
@@ -134,8 +143,6 @@ class BayesianNetwork
     end
   end
 
-
-
   def get_parent_nodes
     parent_nodes = @nodes.reject do |node|
       node.parents != []
@@ -144,8 +151,39 @@ class BayesianNetwork
     parent_nodes
   end
 
+  def reset_network_values
+    @nodes.each do |node|
+      node.value = nil
+    end
+  end
 
+  def fix_event_values(events)
+    events.each do |k, v|
+      @nodes.each do |node|
+        node.value = v if node.name == k
+      end
+    end
+  end
+
+  def normalize_over(x, weighted_counts)
+    a = [0,0]
+    weighted_counts.each do |k, v|
+      if k[x] == true
+        a[0] += v
+      else
+        a[1] += v
+      end
+    end
+
+    total = a[0] + a[1]
+    a = a.map do |e|
+      (e.to_f / total).round(3)
+    end
+    a
+  end
 end
+
+# -------------------------------------------------------------------------
 
 bn = BayesianNetwork.new()
 cloudy    = Node.new("cloudy")
@@ -157,12 +195,6 @@ cloudy.add_child(sprinkler)
 cloudy.add_child(rain)
 sprinkler.add_child(wet_grass)
 rain.add_child(wet_grass)
-
-puts "hippotpoatoia"
-bn.nodes.each do |what|
-  puts what.name.inspect
-end
-
 
 cloudy.set_prob_table(   {{"cloudy" => true}  => 0.5,
                           {"cloudy" => false} => 0.5})
@@ -192,19 +224,37 @@ bn.add_node(rain)
 bn.add_node(sprinkler)
 bn.add_node(wet_grass)
 
-
 rows = bn.set_initial_weighting_count
-puts "weighting done"
-bn.weighted_counts.each do |r|
-  puts r.inspect
+
+n_values = [10, 100, 500, 1000]
+n_values.each do |n_value|
+  puts "n = #{n_value}"
+
+  count = 0
+  sum = [0,0]
+  sum_sqr = [0,0]
+
+  n_value.times do
+    post_prob = bn.likelihood_weighting("cloudy", {"sprinkler" => true, "wet_grass" => true}, n_value)
+    sum[0] += post_prob[0]
+    sum[1] += post_prob[1]
+    count += 1
+
+    sum_sqr[0] += post_prob[0]*post_prob[0]
+    sum_sqr[1] += post_prob[1]*post_prob[1]
+  end
+
+  avg = sum.map do |e|
+    (e.to_f / count).round(3)
+  end
+  puts "Average (true, false): #{avg.inspect}"
+
+  sum_sqr_i = -1
+  var = sum.map do |e|
+    sum_sqr_i += 1
+    ((sum_sqr[sum_sqr_i] - ((e*e)/count.to_f))/(count - 1)).round(4)
+  end
+  puts "Variance (true, false): #{var.inspect}"
 end
-
-    puts "after all THAT BULLISHIT"
-    bn.nodes.each do |what|
-      puts what.name.inspect
-    end
-
-
-# likelihood that it is cloudy given the sprinkler is on and the grass is wet, over 10 samples
-bn.likelihood_weighting("cloudy", {"sprinkler" => true, "wet_grass" => true}, 10)
-
+#puts "post_prob"
+#puts post_prob.inspect
